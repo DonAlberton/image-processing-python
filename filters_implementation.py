fe09e9f0f67bfe8a2433790d5e325153f54f1f6b
dkostycz <01169307@pw.edu.pl>
2023-06-11 16:12:14 +0200

    update filters_implementation

--- filters_implementation.py
+++ filters_implementation.py
@@ -67,7 +67,27 @@ def seasons_filter(image_name):
     confidence_score = prediction[0][index]
 
     return confidence_score, class_name[2:-1]
+def glasses_filter(image_name):
+    np.set_printoptions(suppress=True)
+    model = load_model("glasses_model/keras_model.h5", compile=False)
+    class_names = open("glasses_model/labels.txt", "r").readlines()
 
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    image = Image.open(image_name).convert("RGB")
+
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.LANCZOS)
+
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    return confidence_score, class_name[2:-1]
 
 def face_detection(image):
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
@@ -77,30 +97,26 @@ def face_detection(image):
     for (x, y, width, height) in faces:
         cv2.rectangle(img, (x, y), (x + width, y + height), (255, 0, 0), 3)
 
-    has_faces = False
-    if faces.size>0:
-        has_faces = True
 
-    return img, has_faces
+    return img, faces.size>0,
+
+def show_image(iamge):
+    cv2.imshow("Faces Detected", iamge)
+    cv2.waitKey(0)
 
 
 
-#indoor_images_filter("resources/test_photos/indoor_photo.jpg")
-#indoor_images_filter("resources/test_photos/Mountain-Valid.jpeg")
-#beach_images_filter("resources/test_photos/beach_1.jpg")
-#beach_images_filter("resources/test_photos/desert.jpeg")
-#seasons_filter("resources/test_photos/test-zima.jpg")
 
 def classify_given_image(image):
     is_winter = True if seasons_filter(image)[1] == 'zima' else False
     is_beach = True if beach_images_filter(image)[1] == 'beach' else False
     is_indoor = True if indoor_images_filter(image)[1] == 'indoor_photo' else False
+    contains_glasses = True if glasses_filter(image)[1] == 'Z okularami' else False
     has_faces = face_detection(image)[1]
-    image_classification = {'isWinter': is_winter, 'isBeach': is_beach, 'isIndoor': is_indoor, 'hasFaced': has_faces}
+    image_classification = {'isWinter': is_winter, 'isBeach': is_beach, 'isIndoor': is_indoor, 'hasFaced': has_faces, 'glasses': contains_glasses}
 
     return image_classification
 
 if __name__ == "__main__":
-    #print(classify_given_image("resources/test_photos/test-zima.jpg"))
-    #face_detection("resources/test_photos/test-zima.jpg")
     print(classify_given_image("resources/test_photos/beach_people.png"))
+    show_image(face_detection("resources/test_photos/beach_people.png")[0])
